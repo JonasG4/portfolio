69a7a46bd34a3026b9ab2f73cc4f18e878f0a843
JonasG4 <garciajonas899@gmail.com>
2022-02-06 02:08:27 -0600

    Creando seccion contactos

--- src/Container/Home/Profile.jsx
+++ src/Container/Home/Profile.jsx
@@ -61,7 +61,7 @@ export default function Profile() {
               </span>
             </div>
             <div className="profile-options d-flex flex-row justify-content-center gap-5 my-3">
-              <button className="btn btn-mgreen text-light"> Contactar </button>
+              <a className="btn btn-mgreen text-light" href="#contacto">Contactar </a>
               <a href="CV_JONAS_GARCIA.pdf" download="CV_Jonas_Garcia.pdf">
                 <button className="btn btn-outline-mgreen">
                   Descargar CV <span><i className="fas fa-download"></i></span>
@@ -90,10 +90,10 @@ export default function Profile() {
         <div className="aboutMe-parent d-flex flex-row">
           <div className="card mb-3 card-aboutMe">
             <div className="row g-0">
-              <div className="col-md-4 d-flex justify-content-center align-items-center">
-                <img src="img/bio.jpg" className="img-fluid rounded-start" alt="..." />
+              <div className="col-md-5 d-flex justify-content-center align-items-center">
+                <img src="img/bio.jpg" className="img-fluid rounded" alt="..." />
               </div>
-              <div className="col-md-8">
+              <div className="col-md-7">
                 <div className="card-body px-5 pt-4">
                   <p className="card-text text-justify">
                     Soy un desarrollador web full stack que vive en San Miguel, El Salvador. Actualmente curso el 5to año de la carrera de Ingenieria en Desarrollo Software en la Universidad de Oriente (UNIVO).
@@ -123,8 +123,8 @@ export default function Profile() {
           </div>
         </section>
         <div className="d-flex align-align-items-center">
-          <div className="container-fluid d-flex justify-content-center portfolio-body">
-            <div className="nav flex-column nav-pills me-3 bg-dblue p-3 rounded-3 align-items-center h-100 justify-content-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+          <div className="container-fluid d-flex justify-content-center portfolio-body flex-sm-column flex-lg-row flex-md-row flex-column">
+            <div className="nav flex-column nav-pills  bg-dblue p-3 rounded-3 align-items-center justify-content-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
               <button className="nav-link btn-link active" id="v-pills-skills-tab" data-bs-toggle="pill" data-bs-target="#v-pills-skills" type="button" role="tab" aria-controls="v-pills-skills" aria-selected="true">Habilidades</button>
               <button className="nav-link btn-link" id="v-pills-proyects-tab" data-bs-toggle="pill" data-bs-target="#v-pills-proyects" type="button" role="tab" aria-controls="v-pills-proyects" aria-selected="false">Proyectos</button>
             </div>
@@ -246,30 +246,32 @@ export default function Profile() {
                       Mongo DB
                     </h5>
                     <div className="progress">
-                      <div className="progress-bar bg-mgreen w-75" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                      <div className="progress-bar bg-mgreen w-50" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="tab-pane fade" id="v-pills-proyects" role="tabpanel" aria-labelledby="v-pills-proyects-tab">
+              <div className="tab-pane fade " id="v-pills-proyects" role="tabpanel" aria-labelledby="v-pills-proyects-tab">
                 <div className="row">
-                  <div class="card col">
-                    <img src="img/ohara.png" className="card-img-top" alt="..." />  
+                  <div className="card col-12 col-lg col-sm-12">
+                    <img src="img/ohara.png" className="card-img-top card-img-h" alt="..." />
                     <div className="card-body">
-                    <h5 className="card-title h6">Sistema bibliotecario OHARA</h5>
-                      <p className="card-text">.</p>
+                      <h5 className="card-title h6">Sistema bibliotecario OHARA</h5>
+                      <p className="card-text">Usando PHP puro creamos un sistema para automatizar todos los procesos de una biblioteca.</p>
                     </div>
                   </div>
-                  <div class="card col">
-                    <img src="..." className="card-img-top" alt="..." />
+                  <div className="card col-12 col-lg col-sm-12">
+                    <img src="img/moneyexpress.png" className="card-img-top card-img-h" alt="..." />
                     <div className="card-body">
-                      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <h5 className="card-title h6">Gestión de cartera MoneyExpress</h5>
+                      <p className="card-text">Aplicacion android para gestionar las finanzas usando Java y Firebase para su creación.</p>
                     </div>
                   </div>
-                  <div className="card col">
-                    <img src="..." class="card-img-top" alt="..." />
+                  <div className="card col-12 col-lg col-sm-12">
+                    <img src="img/SRF.jpg" class="card-img-top card-img-h" alt="..." />
                     <div className="card-body">
-                      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <h5 className="card-title h6">Sistema de Registro Familiar</h5>
+                      <p className="card-text">Forme parte de un proyecto freelance para una alcaldia que necesitaba renovar su sistema de registro familiar. Usamos PHP con Sympony y MySQL.</p>
                     </div>
                   </div>
                 </div>
@@ -277,6 +279,39 @@ export default function Profile() {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="contacto mt-5" id="contacto">
+        <section className="heading-container d-flex justify-content-center align-items-center flex-column w-100">
+          <h2 className="heading-title text-dblue fw-bold">Contáctame</h2>
+          <h5 className="heading-subtitle fw-light">Contacto directo</h5>
+          <div className="heading-separator">
+            <div className="separator-line"></div>
+            <div className="separator-blob">
+              <div className="blob-shape"></div>
+            </div>
+          </div>
+        </section>
+
+        <div className="container-fluid w-25 bg-mblue p-4 rounded mb-5">
+        <form action="">
+        <div class="mb-3">
+            <label class="form-label text-light">Nombre</label>
+            <input type="text" className="form-control" placeholder="Escribe tu nombre..." />
+          </div>
+          <div class="mb-3">
+            <label class="form-label text-light">Correo Electronico</label>
+            <input type="email" className="form-control" placeholder="Escribe tu correo electronico..." />
+          </div>
+          <div className="mb-3">
+            <label class="form-label text-light">Mensaje</label>
+            <textarea className="form-control" rows="3">Empieza a escribir tu mensaje...</textarea>
+          </div>
+          <div className="mb-3 d-flex w-100 justify-content-center">
+            <button type="submit" className="btn btn-mgreen">Enviar</button>
+          </div>
+        </form>
         </div>
       </div>
 
